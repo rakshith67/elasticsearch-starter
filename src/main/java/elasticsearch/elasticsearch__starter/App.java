410b3631975f60a3a41b7f8bd1e1ae9a0b884c91
Rakshith <rakshithmamidala67@gmail.com>
2019-12-05 17:33:41 +0530

    First Commit

---
+++ src/main/java/elasticsearch/elasticsearch__starter/App.java
@@ -0,0 +1,13 @@
+package elasticsearch.elasticsearch__starter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
